f6b8af1e83f9b77ecf748590a9000ab66a68994e
jgeovani <geovani.santos74@gmail.com>
2016-10-20 10:10:21 -0300

    feat: 3 tabs

--- app/src/main/java/com/udacity/musicalstructure/MainActivity.java
+++ app/src/main/java/com/udacity/musicalstructure/MainActivity.java
@@ -1,13 +1,113 @@
 package com.udacity.musicalstructure;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        //
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
     }
+
+
+    public static class PlaceholderFragment extends Fragment
+    {
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment()
+        {
+        }
+
+        public static PlaceholderFragment newInstance(int sectionNumber)
+        {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter
+    {
+        List<String> tabsName = Arrays.asList(
+                "Como quero me sentir?",
+                "O que estou fazendo?",
+                "Minhas Favoritas"
+        );
+
+        public SectionsPagerAdapter(FragmentManager fm)
+        {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position)
+        {
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount()
+        {
+            return tabsName.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position)
+        {
+            switch (position)
+            {
+                case 0:
+                    return tabsName.get(0);
+                case 1:
+                    return tabsName.get(1);
+                case 2:
+                    return tabsName.get(2);
+            }
+            return null;
+        }
+    }
+
+
+
 }
